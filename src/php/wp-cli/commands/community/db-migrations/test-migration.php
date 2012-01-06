f590a518c6d4f9208684e3548a4713cb4ff8f8b3
Pauli Price <pauli.price@gmail.com>
2012-01-06 01:23:37 -0500

    basic functionality

--- src/php/wp-cli/commands/community/db-migrations/test-migration.php
+++ src/php/wp-cli/commands/community/db-migrations/test-migration.php
@@ -1,46 +1,46 @@
 <?php
 class TestMigration extends WP_CLI_Migration {
-  
-  public static $type = 'theme';
-  public static $name = 'corporate';
-  
-  /**
-   * Generate current settings of a plugin
-   *
-   * @param array $args
-   */
-  function generate() {
-  	$exec = DbMigrationCommand::connect_string();
-  	$query = "SELECT * FROM wp_rg_form";
-    $result = mysql_query($query);
 	
-    while ($line = mysql_fetch_array($result))
-    {
-      $data[]=$line;
-    }
-    return $data;
-  }
+	public static $type = 'theme';
+	public static $name = 'corporate';
+	
+	/**
+	 * Generate current settings of a plugin
+	 *
+	 * @param array $args
+	 */
+	function generate() {
+		$exec = DbMigrationCommand::connect_string();
+		$query = "SELECT * FROM wp_rg_form";
+		$result = mysql_query($query);
+	
+		while ($line = mysql_fetch_array($result))
+		{
+			$data[]=$line;
+		}
+		return $data;
+	}
 
 
-  /**
-   * Import settings of a plugin
-   *
-   * @param array $args
-   */
-  function migrate(){
-  	list( $file, $name ) = $this->parse_name( $args, __FUNCTION__ );
+	/**
+	 * Import settings of a plugin
+	 *
+	 * @param array $args
+	 */
+	function migrate(){
+		list( $file, $name ) = $this->parse_name( $args, __FUNCTION__ );
 
-  	if ( is_plugin_active( $file ) ) {
-  		$this->deactivate( $args );
-  		// import YAML data
-  		$this->activate( $args );
-  	}
+		if ( is_plugin_active( $file ) ) {
+			$this->deactivate( $args );
+			// import YAML data
+			$this->activate( $args );
+		}
 
-  }
+	}
 }
 
 DbMigrationCommand::add_migration_generator(
-  TestMigration::$type, TestMigration::$name, array( 'TestMigration', 'generate' ));
-        
+	TestMigration::$type, TestMigration::$name, array( 'TestMigration', 'generate' ));
+				
 DbMigrationCommand::add_migration(
-  TestMigration::$type, TestMigration::$name, array( 'TestMigration', 'migrate' ));  
+	TestMigration::$type, TestMigration::$name, array( 'TestMigration', 'migrate' ));	 
